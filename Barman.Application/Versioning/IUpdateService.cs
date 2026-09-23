ad829a186f33c913ecc84247a76f22aa8994a845
Esmaeil Babanezhad <esmaeil.babanezhad@gmail.com>
2026-09-23 17:58:34 +0330

    Add application about and update foundation

---
+++ Barman.Application/Versioning/IUpdateService.cs
@@ -0,0 +1,7 @@
+﻿namespace Barman.Application.Versioning;
+
+public interface IUpdateService
+{
+    Task<UpdateInfo> CheckForUpdateAsync(
+        CancellationToken cancellationToken = default);
+}
---
+++ Barman.Application/Versioning/UpdateInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Barman.Application.Versioning;
+
+public sealed class UpdateInfo
+{
+    public bool IsUpdateAvailable { get; init; }
+
+    public string CurrentVersion { get; init; } = string.Empty;
+
+    public string LatestVersion { get; init; } = string.Empty;
+
+    public string? ReleaseDate { get; init; }
+
+    public string? ReleaseNotes { get; init; }
+
+    public string? DownloadUrl { get; init; }
+}
