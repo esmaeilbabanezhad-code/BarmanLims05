a30db8811da255e4648cf5fffa25915f2390a0ab
Esmaeil Babanezhad <esmaeil.babanezhad@gmail.com>
2026-09-23 17:34:29 +0330

    Add application versioning foundation

---
+++ Barman.Application/Versioning/BarmanVersion.cs
@@ -0,0 +1,8 @@
+﻿namespace Barman.Application.Versioning;
+
+public static class BarmanVersion
+{
+    public const string Current = "1.0.0";
+
+    public const string ProductName = "Barman LIMS";
+}
